5484112a6e12595cea1a799592016b94872394e6
Milovan99 <milovanjakovljevic78@gmail.com>
2022-02-24 12:45:41 +0100

    Learning methods and if statment

---
+++ Learning06/Learning06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Biggest number is : " + GetMax(num1, num2, num3));
+            Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            if( num1 > num2 && num1 > num3)
+            {
+                return num1;
+            }else if(num2 >num1 && num2 > num3)
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
+    }
+}
