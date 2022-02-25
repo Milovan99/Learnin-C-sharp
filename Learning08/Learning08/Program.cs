18ea51e5a86a5bc264734b355e77156e7c26a0b8
Milovan99 <milovanjakovljevic78@gmail.com>
2022-02-25 11:46:21 +0100

    Guessing game

---
+++ Learning08/Learning08/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "pogodi";
+            string guess = "";
+            int num = 5;
+            Console.WriteLine("Broj pokusaka " +num+". Pokusaj pogoditi rec:");
+            guess = Console.ReadLine();
+            while (guess != word && num > 1)
+            {
+               
+                Console.WriteLine("Broj pokusaja "+(num-1)+". Pokusaj ponovo pogoditi rec:");
+                guess = Console.ReadLine();
+                num--;
+            }
+            if (guess == word)
+            {
+                Console.WriteLine("Pogodak iz " + num + ". pokusaja");
+            }
+            else
+            {
+                Console.WriteLine("Niste uspeli ,pokusajte ponovo");
+            }
+            Console.ReadLine();
+        }
+    }
+}
