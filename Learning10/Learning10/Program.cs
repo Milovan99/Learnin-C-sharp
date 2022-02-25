0c04770bb0a8b05cbb9f8c45b1ba0a9a5e37ac70
Milovan99 <milovanjakovljevic78@gmail.com>
2022-02-25 20:33:22 +0100

    Wrapper Class

---
+++ Learning10/Learning10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Collections;
+
+using System.Data.SqlClient;
+
+using System.Data;
+
+using System.Diagnostics;
+
+
+
+namespace Learning10
+
+{
+    class Wrapper //Wrapper Class
+
+    {
+        private class InnerClass //Inner Class
+
+        {
+            public Int32 ProcessData(int Val)
+
+            {
+
+                return Val + 1;
+
+            }
+
+        }
+
+        public Int32 CallRapper()
+
+        {
+            Wrapper.InnerClass wpr = new Wrapper.InnerClass(); // Internally call to Wrapper class.
+            return wpr.ProcessData(100);
+        }
+
+    }
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            Wrapper w = new Wrapper();
+            Console.WriteLine("Data Process by inner class and show by Wrapper Class:" + w.CallRapper());
+            Console.ReadLine();
+        }
+    }
+}
