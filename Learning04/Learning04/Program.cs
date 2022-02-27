10b52410646b5e4e1007ed412421904bea6cd290
Milovan99 <milovanjakovljevic78@gmail.com>
2022-02-27 13:14:44 +0100

    Sort arry

---
+++ Learning04/Learning04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = { 5,2,8,7,1};
+            int temp = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for(int l = i + 1; l < arr.Length; l++)
+                {
+                    if (arr[i] > arr[l])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[l];
+                        arr[l] = temp;
+                    }
+                }
+            }
+            for(int k = 0; k < arr.Length; k++)
+            {
+                Console.WriteLine(" "+arr[k]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
