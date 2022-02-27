28e79351a837640dc420f41a6c4038bb673109fb
Milovan99 <milovanjakovljevic78@gmail.com>
2022-02-27 19:30:40 +0100

    Variables

--- c sharp recap/c sharp/Program.cs
+++ c sharp recap/c sharp/Program.cs
@@ -10,10 +10,30 @@ namespace c_sharp
     {
         static void Main(string[] args)
         {
+            /* ****** Varijable ****** */
+            Variables person = new Variables();//Kreiranje objekta na osnovu klase Variables
+
+            Console.WriteLine("My name : " + person.name); //Ispis atributa string name
+
+            Console.WriteLine("My age : " + person.age);//Ispis atributa int age
+
+            Console.WriteLine("My grade : " + person.grade);//Ispis atributa double grade
+
+            if (person.student) { Console.WriteLine("I'm student"); } else { Console.WriteLine("I'm not student"); } // na osnovu vrednosti
+            //atributa bool student ispisati odrenjei teks
+
+            Console.WriteLine("Firs letter of my name : " + person.myLetter);//Ispis atributa char myLetter
+
+
             /* ****** Sintaksa ****** */
+            Console.WriteLine("1.Syntax \n");
+
             HelloWorld program = new HelloWorld();   //Kreiranje objekta na osnovu klase HelloWorld
+
             Console.WriteLine(program.PrvaRecenica); //Ispis atributa PrvaRecenica
+
             Console.Write(program.Rec + " " + program.DrugaRec); //Ispis spoja atributa Rec i Druga rec
+
             Console.ReadLine();
         }
     }
---
+++ c sharp recap/c sharp/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    class Variables
+    {
+        public string name="Milovan Jakovljevic";
+        public int age = 21;
+        public double grade =8.5;
+        public bool student = true;
+        public char myLetter = 'M';
+
+    }
+}
