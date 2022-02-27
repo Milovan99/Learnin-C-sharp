a3b943244a13453eebc9d5d80515decfd906f4df
Milovan99 <milovanjakovljevic78@gmail.com>
2022-02-27 18:57:36 +0100

    Syntax

---
+++ c sharp recap/c sharp/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    class HelloWorld
+    {
+        public string PrvaRecenica = "Hello World!";
+        public string Rec = "Prvi";
+        public string DrugaRec = "Program";
+       
+    }
+}
---
+++ c sharp recap/c sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ****** Sintaksa ****** */
+            HelloWorld program = new HelloWorld();   //Kreiranje objekta na osnovu klase HelloWorld
+            Console.WriteLine(program.PrvaRecenica); //Ispis atributa PrvaRecenica
+            Console.Write(program.Rec + " " + program.DrugaRec); //Ispis spoja atributa Rec i Druga rec
+            Console.ReadLine();
+        }
+    }
+}
